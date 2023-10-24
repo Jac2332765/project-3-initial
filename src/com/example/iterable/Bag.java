9c8cbecdba16c52736e8c9301a353314500b7fe1
jac2332765 <jac2332765@maricopa.edu>
2023-10-24 16:16:53 -0700

    Final Push/Commit

---
+++ src/com/example/iterable/Bag.java
@@ -0,0 +1,54 @@
+package com.example.iterable;
+
+import java.util.*;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Bag<T> implements Container<T>{
+
+
+    private List<T> bag;
+
+
+
+    public Bag() {
+        this.bag = new ArrayList<>();
+    }
+    @Override
+    public boolean isEmpty() {
+        return bag.isEmpty();
+    }
+    @Override
+    public int size() {
+        return bag.size();
+    }
+    @Override
+    public void add(T item) {
+        bag.add(item);
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return bag.iterator();
+    }
+
+
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        bag.forEach(action);
+    }
+
+
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return bag.spliterator();
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bag{");
+        sb.append("bag=").append(bag);
+        sb.append('}');
+        return sb.toString();
+    }
+}
